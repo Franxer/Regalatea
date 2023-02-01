b57b0d0e93b38a4ff64ddc6a99f711422b9a3bdf
Fran Valverde <fjvalverde@iccaweb.com>
2023-02-01 13:40:55 +0100

    Interfaz Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './shared/servicios/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'regalatea-ng';
+  title = 'Regalatea';
+
+  constructor(private _authenService:AuthenticationService){
+
+  }
+
+  public loginChecked():boolean{
+    return this._authenService.getLoginCheck();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/publico/login/login/login.component';
+import { PrincipalComponent } from './layout/privado/principal/principal/principal.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
